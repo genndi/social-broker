d740e06806f8f448fc9cbff15b0d1d9e026e7f51
genndi <gustavo.enndi.m@gmail.com>
2021-12-02 11:33:21 -0300

    Iniciando o projeto Spring boot com Java 17

---
+++ src/main/java/com/enndi/socialbroker/SocialBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.enndi.socialbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enndi/socialbroker/SocialBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enndi.socialbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
